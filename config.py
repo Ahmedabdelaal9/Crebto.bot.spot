ccc6871b51c196b66bcf6485dae63913c3682915
Ahmedabdelaal9 <ai.99990x@gmail.com>
2025-07-12 01:59:10 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+TELEGRAM_TOKEN = "7850131949:AAH8V5-MrmeImNLjcixVFe22yILxun-7qQI"
+CHANNEL_ID = "@a79909"
+
+BINANCE_API_KEY = "مفتاح API من Binance (اختياري)"
+BINANCE_API_SECRET = "السر الخاص بمفتاح API (اختياري)"
